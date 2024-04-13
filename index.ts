c835146c91453af4a9dd85a7c7c8c1af03bc2785
noor-003 <165676935+noor-003@users.noreply.github.com>
2024-04-14 01:05:41 +0500

    WC
    Project 5

---
+++ index.ts
@@ -0,0 +1,37 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'paragraph',
+      message: 'Enter a paragraph:',
+    },
+  ])
+  .then((answers) => {
+    const paragraph = answers.paragraph;
+    let words = 0;
+    let characters = 0;
+    let inWord = false;
+
+    for (let i = 0; i < paragraph.length; i++) {
+      const char = paragraph[i];
+
+      if (char === ' ') {
+        inWord = false;
+        continue;
+      }
+
+      if (!inWord) {
+        words++;
+        inWord = true;
+      }
+
+      if (char !== ' ') {
+        characters++;
+      }
+    }
+
+    console.log(`Number of words: ${words}`);
+    console.log(`Number of characters: ${characters}`);
+  });
